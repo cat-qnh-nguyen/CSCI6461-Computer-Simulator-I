882f447de0e2cbb9cf72ca6641b66fff7306a1d6
bibinguyen15 <hnguyen1376@student.cccd.edu>
2022-09-25 13:29:27 -0400

    Added constructor

--- src/Memory.java
+++ src/Memory.java
@@ -1,13 +1,15 @@
-
 public class Memory {
     /*
     * Memory of 2048 words, expandable to 4096 words
     * Each word is 2 bytes, which is the size of short in Java
     */
 	// The size of the memory can be changed later to 4096 
-	public static short[] memory = new short[2048];
+	public short[] memory;
 	
 	public Memory() {}
+	public Memory(int size) {
+		memory = new short[size];
+	}
 	
 	public short load(int index) {
 		return memory[index];
