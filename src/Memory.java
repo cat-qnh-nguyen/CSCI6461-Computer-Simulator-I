26036b915ab257a0e272897a7e189d6278220fee
bibinguyen15 <hnguyen1376@student.cccd.edu>
2022-09-25 01:12:58 -0400

    Commit Memory class

--- src/Memory.java
+++ src/Memory.java
@@ -3,8 +3,17 @@ public class Memory {
     /*
     * Memory of 2048 words, expandable to 4096 words
     * Each word is 2 bytes, which is the size of short in Java
-    * 
     */
-    short[] newMemory = new short[2048];
-    
+	// The size of the memory can be changed later to 4096 
+	public static short[] memory = new short[2048];
+	
+	public Memory() {}
+	
+	public short load(int index) {
+		return memory[index];
+	}
+	
+	public void store(int index, short content) {
+		memory[index] = content;
+	}
 }
