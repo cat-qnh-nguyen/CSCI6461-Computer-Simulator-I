138f3d497ab38c3ef159ab77bbc708ea0ec8f456
Aditya Kumar <iadityar25@gmail.com>
2022-10-29 14:58:09 -0400

    Cache Block

--- src/Cache.java
+++ src/Cache.java
@@ -5,64 +5,78 @@ public class Cache {
 	
 	//Creating an Instance to ensure only one memory object is created
 	private static Cache INSTANCE = new Cache();
-	
-	
-	/*Singleton function to access the cache class
-	public static Cache getInstance() {
-		if(INSTANCE == null)
-			INSTANCE = new Cache(2048);
-		return INSTANCE;
-	}*/
+   
+    public int[] cache_tag = new int [16];
+    public int[] cache_valid = new int [16];
+    public int[][] cache_data = new int [16][4]; 
+    public int tag;
+    public int block;
+    public int cache_pointer=0;
 
-
-    /**
-     * Loads the instruction from the cache
-     *
-     * @param key of the instruction is the address from which the instruction is to be fetched from the cache
-     * @return is the value at the given key in the cache
-     */
-
-    String loadFromCache(String key) {
-        long startTime = System.nanoTime();
-        String value = "";
-        if (cache.containsKey(key)) 
+    public static int LoadCache(int address)
+    {   
+        //Check for space in cache, and if founf full, we reset the pointer to 0 thereby implementing a FIFO
+        if(cache_pointer==16)
         {
-            value = cache.get(key);
-        } 
-        else //key does not exists
-        {
-            if (cache.size()==16) {
-                String temp = cache.entrySet().iterator().next().getKey();
-                cache.remove(temp);
+            cache_pointer=0;
+        }
+
+        //Exteracting the data from memory
+        String add=Operations.numToStr(address);
+        tag=Operations.strToNum(add.substring(0,14));
+        block=Operations.strToNum(add.substring(14,16));
+
+        //Search for the existence of tag in the cache 
+        for(int i=0; i<16; i++)
+        {    
+            //If tag is found and has a valid ID
+            if((cache_tag[i] == tag)&&(cache_valid==1))
+            {    
+                // return the data bit
+                return cache_data[i][block]
+            }  
+
+            //if tag is found and does not have a valid ID
+            else if((cache_tag[i] == tag)&&(cache_valid==0))
+            {
+                //retrieve the data and update the valid bit value
+                cache_valid[i]=1;
+                cache_data[i][block]=memory.load(address);
+                return cache_data[i][block]
             }
-            value = loadFromMemeory(key);
-            cache.put(key,value);
-            //System.out.println("Size of cache----->"+cache.size());
+
+            //If tag is not found in the cache at all
+            else
+            {
+                //Store the tag in cache
+                cache_tag[cache_pointer]=tag;
+                cache_valid[cache_pointer]=1;
+                cache_data[cache_pointer][block]=memory.load(address);
+                cache_pointer++;
+                return cache_data[cache_pointer][block];
+            }
+        }       
+    }    
+
+    public static int WriteCache(int address,int data)
+    {
+        //Check for space in cache, and if founf full, we reset the pointer to 0 thereby implementing a FIFO
+        if(cache_pointer==16)
+        {
+            cache_pointer=0;
         }
-        long endTime = System.nanoTime();
-        System.out.println("Time taken to load from Cache: "+(endTime - startTime) + " ns");
-        return value;
+
+        //Exteracting the data from memory
+        String add=Operations.numToStr(address);
+        tag=Operations.strToNum(add.substring(0,14));
+        block=Operations.strToNum(add.substring(14,16));
+
+        //Store the tag in cache
+        cache_tag[cache_pointer]=tag;
+        cache_valid[cache_pointer]=1;
+        cache_data[cache_pointer][block]=data;
+        cache_pointer++;
     }
-
-    /**
-     * Stores the instruction into the cache
-     *
-     * @param key,value specifies the address and the instruction to be stored in the cache
-     */
-    void storeToCache(String key, String value) {
-
-        long startTime = System.nanoTime();
-        if (cache.size()==16) {
-            String temp = cache.entrySet().iterator().next().getKey();
-            cache.remove(temp);
-        }
-
-        cache.put(key,value);
-        long endTime = System.nanoTime();
-        System.out.println("Time taken to store to Cache: "+(endTime - startTime) + " ns");
-        //System.out.println("Size of cache in store----->"+cache.size());
-    }
-
 	
 	
 }
